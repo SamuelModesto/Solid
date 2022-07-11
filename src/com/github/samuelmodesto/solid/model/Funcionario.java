5c255006323aa7e52f6b7d893416469a6072e65a
Samuel Modesto <samuelmodestoes@gmail.com>
2022-07-11 12:35:00 -0300

    S
    single responsibility principle

--- src/com/github/samuelmodesto/solid/model/Funcionario.java
+++ src/com/github/samuelmodesto/solid/model/Funcionario.java
@@ -21,12 +21,8 @@ public class Funcionario {
 		this.salario = salario;
 	}
 
-	public void reajustarSalario(BigDecimal aumento) {
-		BigDecimal percentualReajuste = aumento.divide(salario, RoundingMode.HALF_UP);
-		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-		}
-		this.salario = this.salario.add(aumento);
+	public void atualizarSalario(BigDecimal novoSalario) {
+		this.salario = novoSalario;
 		this.dataUltimoReajuste = LocalDate.now();
 	}
 
