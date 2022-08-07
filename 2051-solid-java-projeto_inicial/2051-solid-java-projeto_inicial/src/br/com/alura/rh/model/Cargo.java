044a4e557a96e420bec4cb35a37ca1bce8eeb6e2
Samuel Modesto <samuelmodestoes@gmail.com>
2022-08-07 20:16:00 -0300

    iniciando o projeto para implementação separada de cada principio solid

---
+++ 2051-solid-java-projeto_inicial/2051-solid-java-projeto_inicial/src/br/com/alura/rh/model/Cargo.java
@@ -0,0 +1,10 @@
+package br.com.alura.rh.model;
+
+public enum Cargo {
+
+	ASSISTENTE,
+	ANALISTA,
+	ESPECIALISTA,
+	GERENTE;
+
+}
---
+++ SingleResponsability/src/main/java/exceptions/ValidacaoException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class ValidacaoException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ValidacaoException(String mensagem) {
+        super(mensagem);
+    }
+
+}
